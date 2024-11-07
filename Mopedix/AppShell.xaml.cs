bce2d70cbd6a64f5d957a06821497cf0f6a6d492
baron <baron@NB555>
2024-11-07 13:38:22 +0100

    Add project files.

---
+++ Mopedix/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mopedix
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Mopedix/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Mopedix
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
